fb8b84986328a887a4d64b5a1e3a5bb14bce2ec2
yiming <yim1ng@outlook.com>
2024-07-19 10:16:45 +0800

    final Assignment2

--- Assignment2/linear_classifier.py
+++ Assignment2/linear_classifier.py
@@ -142,4 +142,210 @@ def softmax_loss_vectorized(
 
     return loss, dW
 
+def sample_batch(
+        X: torch.Tensor,
+        y: torch.Tensor,
+        num_train: int,
+        batch_size: int
+):
+    random_samples = torch.randint(0, num_train, (batch_size, ))
+    X_batch = X[random_samples]
+    y_batch = y[random_samples]
 
+    return X_batch, y_batch
+
+def train_linear_classifier(
+        loss_func: Callable,
+        W: torch.Tensor,
+        X: torch.Tensor,
+        y: torch.Tensor,
+        learning_rate: float = 1e-3,
+        reg: float = 1e-5,
+        num_iters: int = 100,
+        batch_size: int = 200,
+        verbose: bool = False,
+):
+    num_train, dim = X.shape
+    if W is None:
+        num_class = y.max() + 1
+        W = torch.randn(dim, num_class, device=X.device, dtype=X.dtype) * 0.000001
+    else:
+        num_class = W.shape[1]
+
+    loss_history = []
+    for it in range(num_iters):
+        X_batch, y_batch = sample_batch(X, y, num_train, batch_size)
+
+        loss, grad = loss_func(W, X_batch, y_batch, reg)
+        loss_history.append(loss.item())
+        # 更新权重矩阵
+        W -= learning_rate * grad
+
+        if verbose and it % 100 == 0:
+            print("iteration %d / %d: loss %f" % (it, num_iters, loss))
+
+    return W, loss_history
+
+"""
+    计算得分，同时获得当前类别在所有类别中最大的那一个的索引，即为预测标签
+    y_pred = torch.zeros(X.shape[0])    要对当前输入进来的X的每一个都预测即 = num_train
+"""
+def predict_linear_classifier(W: torch.Tensor, X: torch.Tensor):
+    y_pred = torch.zeros(X.shape[0], dtype=torch.int64)
+    scores = torch.mm(X, W)
+    y_pred = torch.argmax(scores, dim=1)
+
+    return y_pred
+
+
+# Template class modules
+class LinearClassifier:
+    """An abstarct class for the linear classifiers"""
+
+    def __init__(self):
+        random.seed(0)
+        torch.manual_seed(0)
+        self.W = None
+
+    def train(
+        self,
+        X_train: torch.Tensor,
+        y_train: torch.Tensor,
+        learning_rate: float = 1e-3,
+        reg: float = 1e-5,
+        num_iters: int = 100,
+        batch_size: int = 200,
+        verbose: bool = False,
+    ):
+        train_args = (
+            self.loss,
+            self.W,
+            X_train,
+            y_train,
+            learning_rate,
+            reg,
+            num_iters,
+            batch_size,
+            verbose,
+        )
+        self.W, loss_history = train_linear_classifier(*train_args)
+        return loss_history
+
+    def predict(self, X: torch.Tensor):
+        return predict_linear_classifier(self.W, X)
+
+    @abstractmethod
+    def loss(
+        self,
+        W: torch.Tensor,
+        X_batch: torch.Tensor,
+        y_batch: torch.Tensor,
+        reg: float,
+    ):
+
+        raise NotImplementedError
+
+    def _loss(self, X_batch: torch.Tensor, y_batch: torch.Tensor, reg: float):
+        self.loss(self.W, X_batch, y_batch, reg)
+
+    def save(self, path: str):
+        torch.save({"W": self.W}, path)
+        print("Saved in {}".format(path))
+
+    def load(self, path: str):
+        W_dict = torch.load(path, map_location="cpu")
+        self.W = W_dict["W"]
+        if self.W is None:
+            raise Exception("Failed to load your checkpoint")
+        # print("load checkpoint file: {}".format(path))
+
+
+class LinearSVM(LinearClassifier):
+    """A subclass that uses the Multiclass SVM loss function"""
+
+    def loss(
+        self,
+        W: torch.Tensor,
+        X_batch: torch.Tensor,
+        y_batch: torch.Tensor,
+        reg: float,
+    ):
+        return svm_loss_vectorized(W, X_batch, y_batch, reg)
+
+
+class Softmax(LinearClassifier):
+    """A subclass that uses the Softmax + Cross-entropy loss function"""
+
+    def loss(
+        self,
+        W: torch.Tensor,
+        X_batch: torch.Tensor,
+        y_batch: torch.Tensor,
+        reg: float,
+    ):
+        return softmax_loss_vectorized(W, X_batch, y_batch, reg)
+
+"""
+    设定超参数
+"""
+def svm_get_search_params():
+
+    learning_rates = []
+    regularization_strengths = []
+    #见论文：Cyclical Learning Rates for Training Neural Networks
+    learning_rates = [1e-4, 1e-3, 5e-2, 2e-2, 1e-2]
+    regularization_strengths = [1e-3, 1e-2, 1e-1, 1e0, 1e1]
+
+
+    return learning_rates, regularization_strengths
+
+
+def test_one_param_set(
+    cls: LinearClassifier,
+    data_dict: Dict[str, torch.Tensor],
+    learning_rate: float,
+    reg: float,
+    num_iters: int = 2000,
+):
+    """
+        训练一个 LinearClassifier 实例同时返回学习完成的实例在训练集和验证集上的准确率
+    :param cls: 线性分类器实例 - LinearSVM() 和 Softmax()
+    :param data_dict: 键值对 ['X_train', 'y_train', 'X_val', 'y_val']作为键值对的key，值为对应的张量
+    :param learning_rate:
+    :param reg:
+    :param num_iters:   当你不确定验证是否正确时可以设置较小的 num_iters 确定正确后再换位最终的
+    :return:    cls: 经过 num_iters 次迭代的 LinearClassifier 实例
+                train_acc, val_acc: 准确率
+    """
+    #获取训练集，及对应训练标签，验证集，即对应验证标签
+    X_train, y_train = data_dict['X_train'], data_dict['y_train']
+    X_val, y_val = data_dict['X_val'], data_dict['y_val']
+    #调用子类的默认损失函数
+    cls.train(X_train, y_train, learning_rate, reg, num_iters, batch_size=200, verbose=False)
+    #求准确率
+    y_train_pred = cls.predict(X_train)
+    train_acc = (y_train == y_train_pred).double().mean().item() * 100.0
+
+    y_val_pred = cls.predict(X_val)
+    val_acc = (y_val == y_val_pred).double().mean().item() * 100.0
+
+    return cls, train_acc, val_acc
+
+def softmax_get_search_params():
+    """
+    Return candidate hyperparameters for the Softmax model. You should provide
+    at least two param for each, and total grid search combinations
+    should be less than 25.
+
+    Returns:
+    - learning_rates: learning rate candidates, e.g. [1e-3, 1e-2, ...]
+    - regularization_strengths: regularization strengths candidates
+                                e.g. [1e0, 1e1, ...]
+    """
+    learning_rates = []
+    regularization_strengths = []
+
+    learning_rates = [1e-4, 1e-3, 5e-2, 2e-2, 1e-2]
+    regularization_strengths = [1e-3, 1e-2, 1e-1, 1e0, 1e1]
+
+    return learning_rates, regularization_strengths
--- Assignment2/linear_classifier_test.py
+++ Assignment2/linear_classifier_test.py
@@ -1,43 +1,375 @@
-import torch
-import time
 import eecs598
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import statistics
+import random
+import time
+import math
+import os
 from linear_classifier import *
 
+eecs598.reset_seed(0)
 data_dict = eecs598.data.preprocess_cifar10(bias_trick=True, cuda=True, dtype=torch.float64)
 
-# The naive implementation and the vectorized implementation should match, but
-# the vectorized version should still be much faster.
+# print('Train data shape: ', data_dict['X_train'].shape)
+# print('Train labels shape: ', data_dict['y_train'].shape)
+# print('Validation data shape: ', data_dict['X_val'].shape)
+# print('Validation labels shape: ', data_dict['y_val'].shape)
+# print('Test data shape: ', data_dict['X_test'].shape)
+# print('Test labels shape: ', data_dict['y_test'].shape)
+#-----------------------------------------------------------------------------------------------
+#
+# W = torch.randn(3073, 10, dtype=data_dict['X_val'].dtype, device=data_dict['X_val'].device) * 0.0001
+#
+# loss, _grad_ = svm_loss_naive(W, data_dict['X_val'], data_dict['y_val'], 0.000005)
+# print('loss: %f' % (loss, ))
+
+#-----------------------------------------------------------------------------------------------
+# W = 0.0001 * torch.randn(3073, 10, dtype=data_dict['X_val'].dtype, device=data_dict['X_val'].device)
+# X_batch = data_dict['X_val'][:128]
+# y_batch = data_dict['y_val'][:128]
+# reg = 0.000005
+#
+# # Run and time the naive version
+# torch.cuda.synchronize()
+# tic = time.time()
+# loss_naive, grad_naive = svm_loss_naive(W, X_batch, y_batch, reg)
+# torch.cuda.synchronize()
+# toc = time.time()
+# ms_naive = 1000.0 * (toc - tic)
+# print('Naive loss: %e computed in %.2fms' % (loss_naive, ms_naive))
+#
+# # Run and time the vectorized version
+# torch.cuda.synchronize()
+# tic = time.time()
+# # YOUR_TURN: implement the loss part of 'svm_loss_vectorized' function in "linear_classifier.py"
+# loss_vec, _ = svm_loss_vectorized(W, X_batch, y_batch, reg)
+# torch.cuda.synchronize()
+# toc = time.time()
+# ms_vec = 1000.0 * (toc - tic)
+# print('Vectorized loss: %e computed in %.2fms' % (loss_vec, ms_vec))
+#
+# # The losses should match but your vectorized implementation should be much faster.
+# print('Difference: %.2e' % (loss_naive - loss_vec))
+# print('Speedup: %.2fX' % (ms_naive / ms_vec))
+#-----------------------------------------------------------------------------------------------
+# torch.cuda.synchronize()
+# tic = time.time()
+# # 7.5 epoch     num_iters = 1500    每次iteration处理batch_size = 200的数据
+# W, loss_hist = train_linear_classifier(svm_loss_vectorized, None,
+#                                        data_dict['X_train'],
+#                                        data_dict['y_train'],
+#                                        learning_rate=3e-11, reg=2.5e4,
+#                                        num_iters=1500, verbose=True)
+# torch.cuda.synchronize()
+# toc = time.time()
+# print('That took %fs' % (toc - tic))
+#
+# plt.plot(loss_hist, 'o')
+# plt.xlabel('Iteration number')
+# plt.ylabel('Loss value')
+# plt.show()
+#
+# y_train_pred = predict_linear_classifier(W, data_dict['X_train'])
+# train_acc = 100.0 * (data_dict['y_train'] == y_train_pred).double().mean().item()
+# print('Training accuracy: %.2f%%' % train_acc)
+#
+# y_val_pred = predict_linear_classifier(W, data_dict['X_val'])
+# val_acc = 100.0 * (data_dict['y_val'] == y_val_pred).double().mean().item()
+# print('Validation accuracy: %.2f%%' % val_acc)
+#-----------------------------------------------------------------------------------------------
+
+### 参考论文
+learning_rates, regularization_strengths = svm_get_search_params()
+###
+num_models = len(learning_rates) * len(regularization_strengths)
+
+####
+# It is okay to comment out the following conditions when you are working on svm_get_search_params.
+# But, please do not forget to reset back to the original setting once you are done.
+if num_models > 25:
+  raise Exception("Please do not test/submit more than 25 items at once")
+elif num_models < 5:
+  raise Exception("Please present at least 5 parameter sets in your final ipynb")
+####
 
 
-# Use random weights and a minibatch of val data for gradient checking
-eecs598.reset_seed(0)
-W = 0.0001 * torch.randn(3073, 10, dtype=data_dict['X_val'].dtype, device=data_dict['X_val'].device)
-X_batch = data_dict['X_val'][:128]
-y_batch = data_dict['y_val'][:128]
-reg = 0.000005
+i = 0
+# results is dictionary mapping tuples of the form
+# (learning_rate, regularization_strength) to tuples of the form
+# (train_acc, val_acc).
+results = {}
+best_val = -1.0   # The highest validation accuracy that we have seen so far.
+best_svm_model = None # The LinearSVM object that achieved the highest validation rate.
+# num_iters = 100 # number of iterations
+num_iters = 100
 
-# Run and time the naive version
-torch.cuda.synchronize()
-tic = time.time()
-_, grad_naive = svm_loss_naive(W, X_batch, y_batch, 0.000005)
-torch.cuda.synchronize()
-toc = time.time()
-ms_naive = 1000.0 * (toc - tic)
-print('Naive loss and gradient: computed in %.2fms' % ms_naive)
+for lr in learning_rates:
+  for reg in regularization_strengths:
+    i += 1
+    print('Training SVM %d / %d with learning_rate=%e and reg=%e'
+          % (i, num_models, lr, reg))
 
-# Run and time the vectorized version
-torch.cuda.synchronize()
-tic = time.time()
-# YOUR_TURN: implement the gradient part of 'svm_loss_vectorized' function in "linear_classifier.py"
-_, grad_vec = svm_loss_vectorized(W, X_batch, y_batch, 0.000005)
-torch.cuda.synchronize()
-toc = time.time()
-ms_vec = 1000.0 * (toc - tic)
-print('Vectorized loss and gradient: computed in %.2fms' % ms_vec)
+    eecs598.reset_seed(0)
+    ####
+    cand_svm_model, cand_train_acc, cand_val_acc = test_one_param_set(LinearSVM(), data_dict, lr, reg, num_iters)
+    ####
+    if cand_val_acc > best_val:
+      best_val = cand_val_acc
+      best_svm_model = cand_svm_model # save the svm
+    results[(lr, reg)] = (cand_train_acc, cand_val_acc)
 
-# The loss is a single number, so it is easy to compare the values computed
-# by the two implementations. The gradient on the other hand is a tensor, so
-# we use the Frobenius norm to compare them.
-grad_difference = torch.norm(grad_naive - grad_vec, p='fro')
-print('Gradient difference: %.2e' % grad_difference)
-print('Speedup: %.2fX' % (ms_naive / ms_vec))
+
+# Print out results.
+for lr, reg in sorted(results):
+  train_acc, val_acc = results[(lr, reg)]
+  print('lr %e reg %e train accuracy: %f val accuracy: %f' % (
+         lr, reg, train_acc, val_acc))
+
+print('best validation accuracy achieved during cross-validation: %f' % best_val)
+
+# save the best model
+path = os.path.join('D:/PythonProject/UMichLearn/Assignment2', 'svm_best_model.pt')
+best_svm_model.save(path)
+
+x_scatter = [math.log10(x[0]) for x in results]
+y_scatter = [math.log10(x[1]) for x in results]
+
+# plot training accuracy
+marker_size = 100
+colors = [results[x][0] for x in results]
+plt.scatter(x_scatter, y_scatter, marker_size, c=colors, cmap='viridis')
+plt.colorbar()
+plt.xlabel('log learning rate')
+plt.ylabel('log regularization strength')
+plt.title('CIFAR-10 training accuracy')
+plt.gcf().set_size_inches(8, 5)
+plt.show()
+
+# plot validation accuracy
+colors = [results[x][1] for x in results] # default size of markers is 20
+plt.scatter(x_scatter, y_scatter, marker_size, c=colors, cmap='viridis')
+plt.colorbar()
+plt.xlabel('log learning rate')
+plt.ylabel('log regularization strength')
+plt.title('CIFAR-10 validation accuracy')
+plt.gcf().set_size_inches(8, 5)
+plt.show()
+
+y_test_pred = best_svm_model.predict(data_dict['X_test'])
+test_accuracy = torch.mean((data_dict['y_test'] == y_test_pred).double())
+print('linear SVM on raw pixels final test set accuracy: %f' % test_accuracy)
+
+w = best_svm_model.W[:-1,:] # strip out the bias
+w = w.reshape(3, 32, 32, 10)
+w = w.transpose(0, 2).transpose(1, 0)
+
+w_min, w_max = torch.min(w), torch.max(w)
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+for i in range(10):
+  plt.subplot(2, 5, i + 1)
+
+  # Rescale the weights to be between 0 and 255
+  wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+  plt.imshow(wimg.type(torch.uint8).cpu())
+  plt.axis('off')
+  plt.title(classes[i])
+plt.show()
+#-----------------------------------------------------------------------------------------------
+#                                           Softmax                                            #
+#-----------------------------------------------------------------------------------------------
+#
+# W = 0.0001 * torch.randn(3073, 10, dtype=data_dict['X_val'].dtype, device=data_dict['X_val'].device)
+#
+# X_batch = data_dict['X_val'][:128]
+# y_batch = data_dict['y_val'][:128]
+#
+# # YOUR_TURN: Complete the implementation of softmax_loss_naive and implement
+# # a (naive) version of the gradient that uses nested loops.
+# loss, grad = softmax_loss_naive(W, X_batch, y_batch, reg=10.0)
+#
+# # As a rough sanity check, our loss should be something close to log(10.0). ( log(C)  = -log(1 / 10), 对于正确类别由于W小接近于e的0次方，其他类别大致相等)
+# print('loss: %f' % loss)
+# print('sanity check: %f' % (math.log(10.0)))
+# f = lambda w: softmax_loss_naive(w, X_batch, y_batch, reg=10.0)[0]
+# eecs598.grad.grad_check_sparse(f, W, grad, 10)
+#-----------------------------------------------------------------------------------------------
+
+# W = 0.0001 * torch.randn(3073, 10, dtype=data_dict['X_val'].dtype, device=data_dict['X_val'].device)
+# reg = 0.05
+#
+# X_batch = data_dict['X_val'][:128]
+# y_batch = data_dict['y_val'][:128]
+#
+# # Run and time the naive version
+# torch.cuda.synchronize()
+# tic = time.time()
+# loss_naive, grad_naive = softmax_loss_naive(W, X_batch, y_batch, reg)
+# torch.cuda.synchronize()
+# toc = time.time()
+# ms_naive = 1000.0 * (toc - tic)
+# print('naive loss: %e computed in %fs' % (loss_naive, ms_naive))
+#
+# # Run and time the vectorized version
+# # YOUR_TURN: Complete the implementation of softmax_loss_vectorized
+# torch.cuda.synchronize()
+# tic = time.time()
+# loss_vec, grad_vec = softmax_loss_vectorized(W, X_batch, y_batch, reg)
+# torch.cuda.synchronize()
+# toc = time.time()
+# ms_vec = 1000.0 * (toc - tic)
+# print('vectorized loss: %e computed in %fs' % (loss_vec, ms_vec))
+#
+# # we use the Frobenius norm to compare the two versions of the gradient.
+# loss_diff = (loss_naive - loss_vec).abs().item()
+# grad_diff = torch.norm(grad_naive - grad_vec, p='fro')
+# print('Loss difference: %.2e' % loss_diff)
+# print('Gradient difference: %.2e' % grad_diff)
+# print('Speedup: %.2fX' % (ms_naive / ms_vec))
+#-----------------------------------------------------------------------------------------------
+# device = data_dict['X_train'].device
+# dtype = data_dict['X_train'].dtype
+# D = data_dict['X_train'].shape[1]
+# C = 10
+#
+# # YOUR_TURN??: train_linear_classifier should be same as what you've implemented in the SVM section
+# W_ones = torch.ones(D, C, device=device, dtype=dtype)
+# W, loss_hist = train_linear_classifier(softmax_loss_naive, W_ones,
+#                                        data_dict['X_train'],
+#                                        data_dict['y_train'],
+#                                        learning_rate=1e-8, reg=2.5e4,
+#                                        num_iters=1, verbose=True)
+#
+#
+# W_ones = torch.ones(D, C, device=device, dtype=dtype)
+# W, loss_hist = train_linear_classifier(softmax_loss_vectorized, W_ones,
+#                                        data_dict['X_train'],
+#                                        data_dict['y_train'],
+#                                        learning_rate=1e-8, reg=2.5e4,
+#                                        num_iters=1, verbose=True)
+#-----------------------------------------------------------------------------------------------
+# torch.cuda.synchronize()
+# tic = time.time()
+#
+# # YOUR_TURN: train_linear_classifier should be same as what you've implemented in the SVM section
+# W, loss_hist = train_linear_classifier(softmax_loss_vectorized, None,
+#                                        data_dict['X_train'],
+#                                        data_dict['y_train'],
+#                                        learning_rate=1e-10, reg=2.5e4,
+#                                        num_iters=1500, verbose=True)
+#
+# torch.cuda.synchronize()
+# toc = time.time()
+# print('That took %fs' % (toc - tic))
+# plt.plot(loss_hist, 'o')
+# plt.xlabel('Iteration number')
+# plt.ylabel('Loss value')
+# plt.show()
+#
+# # evaluate the performance on both the training and validation set
+# # YOUR_TURN: predict_linear_classifier should be same as what you've implemented before, in the SVM section
+# y_train_pred = predict_linear_classifier(W, data_dict['X_train'])
+# train_acc = 100.0 * (data_dict['y_train'] == y_train_pred).double().mean().item()
+# print('training accuracy: %.2f%%' % train_acc)
+# y_val_pred = predict_linear_classifier(W, data_dict['X_val'])
+# val_acc = 100.0 * (data_dict['y_val'] == y_val_pred).double().mean().item()
+# print('validation accuracy: %.2f%%' % val_acc)
+
+#-----------------------------------------------------------------------------------------------
+
+# # YOUR_TURN: find the best learning_rates and regularization_strengths combination
+# #            in 'softmax_get_search_params'
+# learning_rates, regularization_strengths = softmax_get_search_params()
+# num_models = len(learning_rates) * len(regularization_strengths)
+#
+# ####
+# # It is okay to comment out the following conditions when you are working on svm_get_search_params.
+# # But, please do not forget to reset back to the original setting once you are done.
+# if num_models > 25:
+#   raise Exception("Please do not test/submit more than 25 items at once")
+# elif num_models < 5:
+#   raise Exception("Please present at least 5 parameter sets in your final ipynb")
+# ####
+#
+#
+# i = 0
+# # As before, store your cross-validation results in this dictionary.
+# # The keys should be tuples of (learning_rate, regularization_strength) and
+# # the values should be tuples (train_acc, val_acc)
+# results = {}
+# best_val = -1.0   # The highest validation accuracy that we have seen so far.
+# best_softmax_model = None # The Softmax object that achieved the highest validation rate.
+# num_iters = 100 # number of iterations
+#
+# for lr in learning_rates:
+#   for reg in regularization_strengths:
+#     i += 1
+#     print('Training Softmax %d / %d with learning_rate=%e and reg=%e'
+#           % (i, num_models, lr, reg))
+#
+#     eecs598.reset_seed(0)
+#     cand_softmax_model, cand_train_acc, cand_val_acc = test_one_param_set(Softmax(), data_dict, lr, reg, num_iters)
+#
+#     if cand_val_acc > best_val:
+#       best_val = cand_val_acc
+#       best_softmax_model = cand_softmax_model # save the classifier
+#     results[(lr, reg)] = (cand_train_acc, cand_val_acc)
+#
+#
+# # Print out results.
+# for lr, reg in sorted(results):
+#   train_acc, val_acc = results[(lr, reg)]
+#   print('lr %e reg %e train accuracy: %f val accuracy: %f' % (
+#          lr, reg, train_acc, val_acc))
+#
+# print('best validation accuracy achieved during cross-validation: %f' % best_val)
+#
+# # save the best model
+# path = os.path.join('D:/PythonProject/UMichLearn/Assignment2', 'softmax_best_model.pt')
+# best_softmax_model.save(path)
+#
+# x_scatter = [math.log10(x[0]) for x in results]
+# y_scatter = [math.log10(x[1]) for x in results]
+#
+# # plot training accuracy
+# marker_size = 100
+# colors = [results[x][0] for x in results]
+# plt.scatter(x_scatter, y_scatter, marker_size, c=colors, cmap='viridis')
+# plt.colorbar()
+# plt.xlabel('log learning rate')
+# plt.ylabel('log regularization strength')
+# plt.title('CIFAR-10 training accuracy')
+# plt.gcf().set_size_inches(8, 5)
+# plt.show()
+#
+# # plot validation accuracy
+# colors = [results[x][1] for x in results] # default size of markers is 20
+# plt.scatter(x_scatter, y_scatter, marker_size, c=colors, cmap='viridis')
+# plt.colorbar()
+# plt.xlabel('log learning rate')
+# plt.ylabel('log regularization strength')
+# plt.title('CIFAR-10 validation accuracy')
+# plt.gcf().set_size_inches(8, 5)
+# plt.show()
+#
+# y_test_pred = best_softmax_model.predict(data_dict['X_test'])
+# test_accuracy = torch.mean((data_dict['y_test'] == y_test_pred).double())
+# print('softmax on raw pixels final test set accuracy: %f' % (test_accuracy, ))
+#
+# w = best_softmax_model.W[:-1,:] # strip out the bias
+# w = w.reshape(3, 32, 32, 10)
+# w = w.transpose(0, 2).transpose(1, 0)
+#
+# w_min, w_max = torch.min(w), torch.max(w)
+#
+# classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# for i in range(10):
+#   plt.subplot(2, 5, i + 1)
+#
+#   # Rescale the weights to be between 0 and 255
+#   wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+#   plt.imshow(wimg.type(torch.uint8).cpu())
+#   plt.axis('off')
+#   plt.title(classes[i])
+# plt.show()
