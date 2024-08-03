08179c752f3ed527774835fefd19dfe282083d2b
yiming <yim1ng@outlook.com>
2024-08-03 19:46:22 +0800

    class Conv forward & backward done

--- Assignment3/convolutional_networks.py
+++ Assignment3/convolutional_networks.py
@@ -4,6 +4,7 @@ from fully_connected_networks import Linear, Linear_ReLU, Solver, adam, ReLU
 
 
 class Conv(object):
+
     @staticmethod
     def forward(x, w, b, conv_param):
         # 假设输入数据 x 的形状是 (N, 3, 32, 32)，表示 N 张 32x32 的 RGB 图像
@@ -14,29 +15,67 @@ class Conv(object):
         # 步长 和 0填充
         stride, pad = conv_param['stride'], conv_param['pad']
 
-        # 计算输出数据的高度H' 和宽度W' (确保为整数)
+        # 计算输出数据的高度H' 和宽度W' (注意需确保为整数)
         H_prime = 1 + (H - HH + 2 * pad) // stride
         W_prime = 1 + (W - WW + 2 * pad) // stride
 
         # 初始化输出数据
-        out = torch.zeros((N, C, H_prime, W_prime), dtype=x.dtype, device=x.device)
+        out = torch.zeros((N, F, H_prime, W_prime), dtype=x.dtype, device=x.device)
 
-        # 对输入数据进行0填充保证输出大小相同
+        # 对输入数据进行0填充
         x_padded = torch.nn.functional.pad(x, (pad, pad, pad, pad))
 
-        # 当前处理图片的索引
+        # 图片索引
         for n in range(N):
-            # 滤波器索引
+            # 卷积核索引
             for f in range(F):
-                # 下面两层循环是为了计算卷积核(感受野)在输入图像的起始和结束位置
+                # 当前图片在对应卷积核，滤波器在图像上滑动，每次移动stride个像素,覆盖一个与滤波器大小相同的感受野,进行卷积计算
                 for h_prime in range(H_prime):
                     for w_prime in range(W_prime):
                         h_start, h_end = h_prime * stride, h_prime * stride + HH
                         w_start, w_end = w_prime * stride, w_prime * stride + WW
 
-                        # 从填充后的图像提取当前感受野
                         receptive_field = x_padded[n, :, h_start:h_end, w_start:w_end]
-                        # 计算卷积操作的输出, 将当前感受野与滤波器w[f]对应元素相乘求和加上偏置
                         out[n, f, h_prime, w_prime] = torch.sum(receptive_field * w[f]) + b[f]
+
         cache = (x, w, b, conv_param)
         return out, cache
+
+    @staticmethod
+    def backward(dout, cache):
+
+        # unpack cache
+        x, w, b, conv_param = cache
+        N, C, H, W = x.shape
+        F, C, HH, WW = w.shape
+        stride, pad = conv_param['stride'], conv_param['pad']
+
+        # initialize
+        dx = torch.zeros_like(x)
+        dw = torch.zeros_like(w)
+        db = torch.zeros_like(b)
+
+        N, F, W_prime, H_prime = dout.shape
+
+        #padding
+        x_padded = torch.nn.functional.pad(x, (pad, pad, pad, pad))
+        dx_padded = torch.nn.functional.pad(dx, (pad, pad, pad, pad))
+
+        for n in range(N):
+            for f in range(F):
+                for h_prime in range(H_prime):
+                    for w_prime in range(W_prime):
+                        h_start, h_end = h_prime * stride, h_prime * stride + HH
+                        w_start, w_end = w_prime * stride, w_prime * stride + WW
+
+                        receptive_field = x_padded[n, :, h_start:h_end, w_start:w_end]
+
+                        dx_padded[n, :, h_start:h_end, w_start:w_end] += dout[n, f, h_prime, w_prime] * w[f]
+                        dw[f] += receptive_field * dout[n, f, h_prime, w_prime]
+                        db[f] += dout[n, f, h_prime, w_prime]
+        # unpad data
+        # 从第 pad 个元素开始, 到倒数第 pad 个元素结束: 去掉前 pad 个和后 pad 个元素
+        dx = dx_padded[:, :, pad:-pad, pad:-pad]
+
+        return dx, dw, db
+
