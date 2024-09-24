cdd660a878c2b156187fb257ac5a549f63178e8e
yiming <yim1ng@outlook.com>
2024-09-24 11:09:39 +0800

    Self Attention done

--- Assignment5/transformers.py
+++ Assignment5/transformers.py
@@ -41,34 +41,122 @@ def prepocess_input_sequence(
 
 def scaled_dot_product_two_loop_single(
     query: Tensor, key: Tensor, value: Tensor
-) -> Tensor:
+):
 
     K, M = query.shape
     K_k, M = key.shape
 
-    QK = torch.zeros((K, K_k), dtype=query.dtype, device=query.device)
+    attention_scores = torch.zeros((K, K_k), dtype=query.dtype, device=query.device)
 
-    QK = torch.mm(query, key.transpose(1, 0)) / math.sqrt(M)
-    QK = torch.softmax(QK, dim=-1)
+    for i in range(K):
+        for j in range(K_k):
+            attention_scores[i, j] = torch.inner(query[i], key[j])
 
-    out = torch.mm(QK, value)
+    '''
+    # with no loops dot_product_single
+    attention_scores = torch.mm(query, key.transpose(1, 0))
+    '''
+
+    attention_scores = attention_scores / math.sqrt(M)
+    attention_scores = F.softmax(attention_scores, dim=-1)
+
+    out = torch.mm(attention_scores, value)
 
     return out
 
 def scaled_dot_product_two_loop_batch(
     query: Tensor, key: Tensor, value: Tensor
-) -> Tensor:
+):
 
     N, K, M = query.shape
     N, K_k, M = key.shape
 
-    QK = torch.zeros((N, K, K_k), dtype=query.dtype, device=query.device)
+    attention_scores = torch.zeros((N, K, K_k), dtype=query.dtype, device=query.device)
 
-    QK = torch.bmm(query, key.permute(0, 2, 1))
+    for i in range(K):
+        for j in range(K_k):
+            attention_scores[:, i, j] = (query[:, i] * key[:, j]).sum(dim=-1)
+    '''
+    # with no loops dot_product_batch
+    attention_scores = torch.bmm(query, key.permute(0, 2, 1))
+    '''
 
-    QK = QK / math.sqrt(M)
-    QK = torch.softmax(QK, dim=-1)
+    attention_scores = attention_scores / math.sqrt(M)
+    attention_scores = F.softmax(attention_scores, dim=-1)
 
-    out = torch.bmm(QK, value)
+    out = torch.bmm(attention_scores, value)
 
     return out
+
+def scaled_dot_product_no_loop_batch(
+    query: Tensor, key: Tensor, value: Tensor, mask: Tensor = None
+):
+
+    N, K, M = query.shape
+    N, K_k, M = key.shape
+    y = None
+    weights_softmax = None
+
+    attention_scores = torch.bmm(query, key.permute(0, 2, 1))
+    attention_scores = attention_scores / math.sqrt(M)
+
+    if mask is not None:
+        torch.masked_fill(attention_scores, mask, -1e9)
+
+    weights_softmax = F.softmax(attention_scores, dim=-1)
+    y = torch.bmm(weights_softmax, value)
+
+    return y, weights_softmax
+
+class SelfAttention(nn.Module):
+    def __init__(self, dim_in: int, dim_q: int, dim_v: int):
+        super().__init__()
+        """
+        args:
+            dim_in: an int value for input sequence embedding dimension
+            dim_q: an int value for output dimension of query and ley vector
+            dim_v: an int value for output dimension for value vectors
+        """
+        self.q = nn.Linear(dim_in, dim_q)
+        self.k = nn.Linear(dim_in, dim_q)
+        self.v = nn.Linear(dim_in, dim_v)
+
+        """
+        If a Linear layer has input dimension D_in and output dimension D_out  
+        then initialize the weights sampled from a uniform distribution bounded
+        by [-c, c]                                                             
+        where c = sqrt(6/(D_in + D_out)) 
+        """
+        # initialize weights
+        c1 = math.sqrt(6 / (dim_in + dim_q))
+        c2 = math.sqrt(6 / (dim_in + dim_v))
+
+        nn.init.uniform_(self.q.weight, a=-c1, b=c1)
+        nn.init.uniform_(self.k.weight, a=-c1, b=c1)
+        nn.init.uniform_(self.v.weight, a=-c2, b=c2)
+
+    def forward(
+            self, query: Tensor, key: Tensor, value: Tensor, mask: Tensor = None
+    ) -> Tensor:
+        """
+        Args:
+            query: Tensor of shape (N, K, M)
+            key: Tensor of shape (N, K, M)
+            value: Tensor of shape (N, K, M)
+            mask: Tensor of shape (N, K, K)
+        Returns:
+            y: Tensor of shape (N, K, dim_v)
+        """
+
+        self.weights_softmax = (
+            None
+        )
+        y = None
+
+        query = self.q(query)
+        key = self.k(key)
+        value = self.v(value)
+
+        y, self.weights_softmax = scaled_dot_product_no_loop_batch(query, key, value, mask)
+
+        return y
--- Assignment5/transformers_test.py
+++ Assignment5/transformers_test.py
@@ -1,4 +1,5 @@
 import os
+import timeit
 
 import torch
 import torch.nn.functional as F
@@ -195,3 +196,134 @@ vocab = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] + SPECIAL_TOKENS
 # ).to(torch.float32)
 # print("scaled_dot_product_two_loop_batch error: ", rel_error(y_expected, y))
 # ---------------------------------------------------------------------------------------------------------
+# N = 2  # Number of sentences
+# K = 5  # Number of words in a sentence
+# M = 4  # feature dimension of each word embedding
+#
+# query = torch.linspace(-0.4, 0.6, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# key = torch.linspace(-0.8, 0.5, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# value = torch.linspace(-0.3, 0.8, steps=N * K * M).reshape(N, K, M)  # *to_double_cuda
+#
+#
+# y, _ = scaled_dot_product_no_loop_batch(query, key, value)
+#
+# y_expected = torch.tensor(
+#     [
+#         [
+#             [-0.09603, -0.06782, -0.03962, -0.01141],
+#             [-0.08991, -0.06170, -0.03350, -0.00529],
+#             [-0.08376, -0.05556, -0.02735, 0.00085],
+#             [-0.07760, -0.04939, -0.02119, 0.00702],
+#             [-0.07143, -0.04322, -0.01502, 0.01319],
+#         ],
+#         [
+#             [0.49884, 0.52705, 0.55525, 0.58346],
+#             [0.50499, 0.53319, 0.56140, 0.58960],
+#             [0.51111, 0.53931, 0.56752, 0.59572],
+#             [0.51718, 0.54539, 0.57359, 0.60180],
+#             [0.52321, 0.55141, 0.57962, 0.60782],
+#         ],
+#     ]
+# ).to(torch.float32)
+#
+# print("scaled_dot_product_no_loop_batch error: ", rel_error(y_expected, y))
+# ---------------------------------------------------------------------------------------------------------
+# N = 64
+# K = 256  # defines the input sequence length
+# M = emb_size = 2048
+# dim_q = dim_k = 2048
+# query = torch.linspace(-0.4, 0.6, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# key = torch.linspace(-0.8, 0.5, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# value = torch.linspace(-0.3, 0.8, steps=N * K * M).reshape(N, K, M)  # *to_double_cuda
+#
+# # 测试的函数
+# def test_function():
+#     y, weights_softmax = scaled_dot_product_no_loop_batch(query, key, value)
+#
+# # 使用 timeit 模块，重复执行 5 次，测量执行时间
+# execution_time = timeit.timeit(test_function, number=5)
+# print(f"Average Execution Time over 5 runs: {execution_time / 5} seconds")
+#
+# N = 64
+# K = 512  # defines the input requence length
+# M = emb_size = 2048
+# dim_q = dim_k = 2048
+# query = torch.linspace(-0.4, 0.6, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# key = torch.linspace(-0.8, 0.5, steps=N * K * M).reshape(N, K, M)  # **to_double_cuda
+# value = torch.linspace(-0.3, 0.8, steps=N * K * M).reshape(N, K, M)  # *to_double_cuda
+#
+# # 测试的函数
+# def test_function():
+#     y, weights_softmax = scaled_dot_product_no_loop_batch(query, key, value)
+#
+# # 使用 timeit 模块，重复执行 5 次，测量执行时间
+# execution_time = timeit.timeit(test_function, number=5)
+# print(f"Average Execution Time over 5 runs: {execution_time / 5} seconds")
+# ---------------------------------------------------------------------------------------------------------
+reset_seed(0)
+N = 2
+K = 4
+M = emb_size = 4
+dim_q = dim_k = 4
+atten_single = SelfAttention(emb_size, dim_q, dim_k)
+
+for k, v in atten_single.named_parameters():
+    # print(k, v.shape) # uncomment this to see the weight shape
+    v.data.copy_(torch.linspace(-1.4, 1.3, steps=v.numel()).reshape(*v.shape))
+
+query = torch.linspace(-0.4, 0.6, steps=N * K * M, requires_grad=True).reshape(
+    N, K, M
+)  # **to_double_cuda
+key = torch.linspace(-0.8, 0.5, steps=N * K * M, requires_grad=True).reshape(
+    N, K, M
+)  # **to_double_cuda
+value = torch.linspace(-0.3, 0.8, steps=N * K * M, requires_grad=True).reshape(
+    N, K, M
+)  # *to_double_cuda
+
+query.retain_grad()
+key.retain_grad()
+value.retain_grad()
+
+y_expected = torch.tensor(
+    [
+        [
+            [-1.10382, -0.37219, 0.35944, 1.09108],
+            [-1.45792, -0.50067, 0.45658, 1.41384],
+            [-1.74349, -0.60428, 0.53493, 1.67414],
+            [-1.92584, -0.67044, 0.58495, 1.84035],
+        ],
+        [
+            [-4.59671, -1.63952, 1.31767, 4.27486],
+            [-4.65586, -1.66098, 1.33390, 4.32877],
+            [-4.69005, -1.67339, 1.34328, 4.35994],
+            [-4.71039, -1.68077, 1.34886, 4.37848],
+        ],
+    ]
+)
+
+dy_expected = torch.tensor(
+    [
+        [
+            [-0.09084, -0.08961, -0.08838, -0.08715],
+            [0.69305, 0.68366, 0.67426, 0.66487],
+            [-0.88989, -0.87783, -0.86576, -0.85370],
+            [0.25859, 0.25509, 0.25158, 0.24808],
+        ],
+        [
+            [-0.05360, -0.05287, -0.05214, -0.05142],
+            [0.11627, 0.11470, 0.11312, 0.11154],
+            [-0.01048, -0.01034, -0.01019, -0.01005],
+            [-0.03908, -0.03855, -0.03802, -0.03749],
+        ],
+    ]
+)
+
+y = atten_single(query, key, value)
+dy = torch.randn(*y.shape)  # , **to_double_cuda
+
+y.backward(dy)
+query_grad = query.grad
+
+print("SelfAttention error: ", rel_error(y_expected, y))
+print("SelfAttention error: ", rel_error(dy_expected, query_grad))
